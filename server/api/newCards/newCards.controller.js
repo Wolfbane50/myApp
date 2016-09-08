4c852afe281d029406815a35b3772f25ec49c076
Heaney <1274185364@mil>
2016-09-08 15:10:51 -0400

    Make newCards server work

--- server/api/newCards/newCards.controller.js
+++ server/api/newCards/newCards.controller.js
@@ -2,12 +2,15 @@
 var fs = require("fs");
  var FindFiles = require("node-find-files");
  var path = require('path');
+ var XLSX = require('xlsx');
+
 
 export function index(req, res) {
   console.log("-------------In newCards server --");
   //console.log("Request ==>\n" + JSON.stringify(req.body));
 
    var cardfiles = [];
+   // This will need to change !!!!!!!!!!!
    var cardDir = "./server/public/cards";
    var pubDir = "server/public/";
    var processNewCard = function (strPath, stat) {
@@ -71,7 +74,87 @@ export function index(req, res) {
 
 export function update(req, res) {
   // Should get data from tagging and update ss_cards.json
-  console.log("Update not implemented yet");
+  function parse_chachi_excel() {
+    var ch_path = "./";
+    var workbook = XLSX.readFile(ch_path + 'newCards.xlsx');
+    var jsonOut = ch_path + "ss_cards.json";
+    // Specify worksheets to process
+    var idSheets = {
+      "Bunt": 1,
+      "Huddle Offseason": 1
+    };
+
+    var chachi_db = [];
+
+    function saveSet(theSet) {
+      if (theSet.cards.length) {
+        chachi_db.push(theSet);
+      }
+    }
+
+
+    for (var ws_num = 0; ws_num < workbook.SheetNames.length; ws_num++) {
+      // Each sheet represents a year/set
+      var sheet_name = workbook.SheetNames[ws_num];
+      //console.log("Sheet = " + sheet_name);
+
+      if (idSheets[sheet_name]) {
+        var chachi_set = {
+          "year": sheet_name,
+          "cards": []
+        };
+        var worksheet = workbook.Sheets[sheet_name];
+
+        // Parse through rows
+        var roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet_name]);
+        roa.forEach(function(row) {
+          if (row.Name) { // Skip rows with no name
+            if (row['File Name']) {
+              if (row['File Name'].match(/^No/)) {
+                //console.log("ID is No: " + row.id);
+                // Could put a default image here
+              } else {
+                // Skip any rows without a url
+                //console.log("name : " + row.name);
+                //console.log("url : " + row.link);
+                var card = {
+                  "name": row.Name,
+                  "fname": row['File Name'],
+                  "type" : row.Type,
+                  "brand" : row.Brand,
+                  "team" : row.Team
+                };
+                chachi_set.cards.push(card);
+              }
+
+            } else {
+              // Name but no Link indicates start of new set
+              saveSet(chachi_set);
+              chachi_set = {
+                "year": row.Name,
+                "cards": []
+              };
+            }
+          }
+
+        });
+        // Save last set on sheet
+        saveSet(chachi_set);
+      }
+    }
+    return chachi_db;
+  }
+
+  var s_path = "server/public/";
+  console.log('Updating ss_cards.json');
+  try {
+    fs.writeFileSync(s_path + 'ss_cards.json',
+      JSON.stringify(parse_chachi_excel()));
+
+  } catch (e) {
+    console.log("Caught exception parsing and writing - " + e);
+  }
+
   res.redirect(303, '/ss_cards.json');
 
 }
