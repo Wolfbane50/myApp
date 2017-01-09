48fa5e54f4da26c36dc063c4907c9ba338426506
Daniel Heaney <daniel.heaney@gmail.com>
2017-01-09 14:50:13 -0500

    Handle navbar for mobile

---
+++ client/components/navbar/navbar.component.js
@@ -0,0 +1,46 @@
+'use strict';
+/* eslint no-sync: 0 */
+
+(function() {
+
+  class NavbarComponent {
+    constructor($location, $scope, $rootScope) {
+        'ngInject';
+
+        this.$location = $location;
+        this.isCollapsed = true;
+        var navScope = this;
+        //console.log("Constructor being called");
+        //this.isLoggedIn = Auth.isLoggedInSync;
+        //this.isAdmin = Auth.isAdminSync;
+        //this.getCurrentUser = Auth.getCurrentUserSync;
+
+        // Collapse the Navbar when changing state.
+        $rootScope.$on('$stateChangeStart',
+           function(event, toState, toParams, fromState, fromParams) {
+             //console.log("Changing state...");
+             navScope.isCollapsed = true;
+           });
+
+
+
+
+
+
+      } //end of contstructor
+
+    $onInit() {
+      //console.log("On init being called");
+      this.isCollapsed = true;
+    }
+
+  }
+
+  angular.module('myappApp')
+    .component('navbar', {
+      templateUrl: 'components/navbar/navbar.html',
+      controller: NavbarComponent
+    });
+
+
+})();
