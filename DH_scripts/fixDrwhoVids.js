6c430c0c314b45d62ed2ea4ea5f6ba416879fc7e
Daniel Heaney <Daniel Heaney>
2017-03-06 06:12:21 -0500

    Got video player working

--- DH_scripts/fixDrwhoVids.js
+++ DH_scripts/fixDrwhoVids.js
@@ -11,7 +11,9 @@ fs.readFile(audConfig, function(err, content) {
     console.log('Could not read JSON file ' + audConfig + ": " + err);
     return;
   }
-  console.log(content.toString().substr(0, 8));
+  var vidType = {};
+  var convCount = 0;
+  console.log("Y:");
 
   audData = JSON.parse(content.toString());
   audData.forEach(function(drElement, index, array) {
@@ -19,61 +21,43 @@ fs.readFile(audConfig, function(err, content) {
     var items = drElement.items;
     items.forEach(function(element, index, array) {
       //console.log("Working on " + element.Title)
-      if (element['S-Path']) {
-        element.SPath = element['S-Path'];
-        delete element['S-Path'];
+      if(element.SPath.match(/^TV Shows/)) {
+        element.SPath = 'http://NASD9760C/' + element.SPath;
       }
 
-      if (element.Released) {
-        if (typeof element.Released == "string") {
-          // Convert ISO 8601 Date string representation ("1999-07-01T00:00:00.000Z") to Javascript Date Object
-          //console.log("Converting date from ISO string to object...");
-          var dtstr = element.Released.replace(/\D/g, " ");
-          var dtcomps = dtstr.split(" ");
-          dtcomps[1]--;
-          element.Released = new Date(Date.UTC(dtcomps[0], dtcomps[1], dtcomps[2], dtcomps[3], dtcomps[4], dtcomps[5]));
-        } else {
+      var dirChanged = false;
+      var localPath = element.SPath.replace(/http:\/\/NASD9760C\//, 'Y:/');
+      localPath = localPath.replace(/\//g, "\\");
+      var chdir = "cd \"" + localPath + "\"";
 
-          if (typeof element.Released == "number") {
-            // Convert from Excel Date Format,
-            //    excel - number of days since Dec 31, 1899
-            //    javascript - number of milliseconds since Jan 1, 1970
-            //    70 years * 365.25 days  ==> pretty close to 25569
-            //console.log("Converting date from excel format to Javascript...");
-            var real_utc_days = element.Released - 25569;
-            if (real_utc_days > 0) {
-              var utc_days = Math.floor(element.Released - 25569);
-              var utc_value = utc_days * 86400;
-              element.Released = new Date(utc_value);
-            } else {
-              var exc_year = Math.floor(element.Released / 365) + 1900;
-              var exc_yday = element.Released % 365;
 
-              //console.log("Excel date: " + element.Released);
-              var mtot = 0;
-              var idx = 0;
-              var mdays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-              var exc_date = 1;
-              while ((idx < mdays.length) && (exc_yday > mtot)) {
-                mtot += mdays[idx];
-                idx++;
-              }
-              var exc_mon = idx - 1;
-              //console.log("Day in year = " + exc_yday + "\nSetting Date to " + exc_mon + '/'+ exc_date + '/' + exc_year)
-              element.Released = new Date(exc_year, exc_mon, exc_date);
-            }
-          } else {
-            alert("Unknown format for release date of " + element.name + " : " + element.Released);
-            throw ("Database format Error");
-          }
-        }
-      }
-
-      // Remove links from episodes
       if ((element.episodes) && (element.episodes.length)) {
         element.episodes.forEach(function(ep, epIndex, epArray) {
-          if (ep.link) {
-            delete ep.link
+          var fname = ep.fname;
+          //var exts = fname.match(/\.*+$/);
+          var ext = fname.substr(fname.lastIndexOf('.'));
+
+          if ((ext == ".mp4") || (ext == ".m4v")) {
+          } else {
+            convCount++;
+            vidType[ext] = true;
+
+            if(! dirChanged) {
+              console.log(chdir);
+              dirChanged = true;
+            }
+             var outfile = fname.replace(ext, ".mp4");
+             var outfile = outfile.replace(/\(/g, '');
+             var outfile = outfile.replace(/\)/g, '');
+
+//             console.log("'C:\\Program Files\\Handbrake\\HandBrakeCLI' -i '" + fname + "' -o '" + outfile +"'");
+             console.log('C:\\ffmpeg-20160310-git-66edd86-win64-static\\bin\\ffmpeg -i "' + fname + '" " ' + outfile + '"');
+
+             ep.fname = outfile;
+            //if (!((ext == ".avi") || (ext ==".mkv"))) {
+            //  console.log(element.SPath + '/' + fname);
+            //}
+
           }
         });
       }
@@ -83,5 +67,8 @@ fs.readFile(audConfig, function(err, content) {
 
 
   });
+  //console.log("Must convert " + convCount + " videos!");
+  //console.log("Video Types: " +  Object.keys(vidType).join('-'));
+
   fs.writeFile("server/public/new_drwho_tv_tree.json", JSON.stringify(audData));
 });
--- client/app/drwhoTree/drwhoTree.component.js
+++ client/app/drwhoTree/drwhoTree.component.js
@@ -3,272 +3,318 @@
 (function() {
 
   class drwhoTreeComponent {
-    constructor($http, $scope, $sce, ngToast) {
-        //console.log("In bftree constructor");
-        this.episodes = [];
-        this.episodeListText = "";
+    constructor($http, $scope, $sce, $timeout, ngToast) {
+      //console.log("In bftree constructor");
+      this.episodes = [];
+      this.episodeListText = "";
 
-        this.selItem = {};
-        this.showShow = false;
-        this.videoList = [];
-        this.editMode = false;
+      this.selItem = {};
+      this.showShow = false;
+      this.videoList = [];
+      this.editMode = false;
 
-        this.featuring = [];
-        this.questionMark = "-851px -417px";
-        this.selectedItem = {};
+      this.featuring = [];
+      this.questionMark = "-851px -417px";
+      this.selectedItem = {};
 
-        this.options = {};
-        this.format = 'MMM yyyy';
+      this.options = {};
+      this.format = 'MMM yyyy';
 
-        this.popup1 = {
-          opened: false
-        };
+      this.popup1 = {
+        opened: false
+      };
 
-        this.$scope = $scope;
-        this.$http = $http;
-        this.ngToast = ngToast;
-        this.$sce = $sce;
+      this.$scope = $scope;
+      this.$http = $http;
+      this.ngToast = ngToast;
+      this.$sce = $sce;
+      this.$timeout = $timeout;
 
-        // Configuration for videogular
-        this.videogular = null;
-        this.vidConfig = {
-          sources: [{
-            src: this.$sce.trustAsResourceUrl("http://127.0.0.1:9000/11%20-%203%20-%20Kd-Trees%20%282907%29.mp4"),
-            type: "video/mp4"
-          }],
-          tracks: [],
-          theme: "bower_components/videogular-themes-default/videogular.css",
-          plugins: {
-            poster: "http://www.videogular.com/assets/images/videogular.png"
-          }
-        };
-        this.onPlayerReady = function(API) {
-          this.videogular = API;
-        };
+      // Configuration for videogular
+      this.videogular = null;
+      this.vidConfig = {
+        sources: null,
+        tracks: null,
+        theme: "bower_components/videogular-themes-default/videogular.css",
+        plugins: {
+          poster: "http://www.videogular.com/assets/images/videogular.png"
+        }
+      };
+      this.onPlayerReady = function(API) {
+        console.log("Video Player is ready!");
+        this.videogular = API;
+      };
 
-        this.vidTypeMap = {
-            ".mp4" : "/video/mp4",
-            ".webm" : "/video/webm",
-            ".ogg" : "/video/ogg"
-        };
+      this.vidTypeMap = {
+        ".mp4": "video/mp4",
+        ".m4v": "video/mp4", 
+        ".webm": "video/webm",
+        ".ogg": "video/ogg",
+        //".mkv" : "video/x-matroska"   // Would work if supported
+        ".mkv": "video/webm", // Hack works in chrome only
+        ".avi" : "video/avi",  // Not supported
+        ".mov" : "video/mov"
+      };
 
-        this.playEpisode = function(index) {
-          this.videogular.stop();
-          console.log("Old video config => " + JSON.stringify(this.vidConfig.sources));
-          var fname = this.selItem.episodes[index].fname
-          //var exts = fname.match(/\.*+$/);
-          var ext = fname.substr(fname.lastIndexOf('.'));
+      this.playEpisode = function(index) {
+        this.videogular.stop();
+        console.log("Old video config => " + JSON.stringify(this.vidConfig.sources));
+        var fname = this.selItem.episodes[index].fname;
+        //var exts = fname.match(/\.*+$/);
+        var ext = fname.substr(fname.lastIndexOf('.'));
+        var vtype = this.vidTypeMap[ext];
+        var canPlay = this.videogular.mediaElement[0].canPlayType(vtype);
+        if (! canPlay) {
+          return alert("Browser cannot play this video!");
+        }
 
-          var url = this.$sce.trustAsResourceUrl(this.selItem.SPath + "/" + this.selItem.episodes[index].fname);
+        var url = this.selItem.SPath + "/" + this.selItem.episodes[index].fname;
 
         this.vidConfig.sources = [{
-            src: this.$sce.trustAsResourceUrl(this.selItem.SPath + "/" + this.selItem.episodes[index].fname),
-            type: this.vidTypeMap[ext]
-          }];
-          console.log("Set video config sources to => " + JSON.stringify(this.vidConfig.sources));
-          this.videogular.changeSource(this.vidConfig.sources);
+          src: this.$sce.trustAsResourceUrl(url),
+          type: vtype
+        }];
+
+        console.log("Set video config sources to => { src : " +
+          $sce.getTrustedResourceUrl(this.vidConfig.sources[0].src) + ", \ntype: " +
+          this.vidConfig.sources[0].type + " }");
+        this.videogular.changeSource(this.vidConfig.sources);
+
+        //  this.$timeout(this.videogular.play.bind(this.videogular), 100);
+        //this.videogular.play();
+
+      };
+      this.onVgError = function(event) {
+        console.log("Player Error! =>  " + event);
+      };
+      this.checkPlayer = function() {
+        console.log("Current player state: " + this.videogular.currentState);
+        var theSources = this.videogular.sources;
+        if ((theSources) && (theSources.length)) {
+          console.log("Current player source = " +
+            $sce.getTrustedResourceUrl(theSources[0].src));
+            console.log("Checing viability of source...");
+
+            if (this.videogular.mediaElement[0].canPlayType) {
+              var canPlay = this.videogular.mediaElement[0].canPlayType(theSources[0].type);
+              console.log("canPlay value  for " + theSources[0].type + " => " + canPlay);
+            } else {
+              console.log("No canPlayType suport!");
+            }
+        } else {
+          console.log("Currently no sources...");
+        }
+
+
+
+      };
+      this.onCompleteVideo = function() {
+        console.log("Completed Episode");
+      };
+      this.onCanPlay = function() {
+        console.log("Video can play!");
         this.videogular.play();
+      }
+
+
+      this.selectedItemClass = function(scope) {
+        var nodeData = scope.$modelValue;
+        if (this.selItem == nodeData) {
+          return 'dhitem-selected';
+        } else {
+          return 'dhitem';
+        }
+      };
+
+      this.itemIcon = function(scope) {
+        if (scope.hasChild()) {
+          return scope.collapsed ? 'glyphicon-folder-close' : 'glyphicon-folder-open';
+        } else {
+          return 'glyphicon-file';
+        }
+      };
+      this.canAdd = function(scope) {
+        return scope.hasChild();
       };
 
 
-        this.selectedItemClass = function(scope) {
-          var nodeData = scope.$modelValue;
-          if (this.selItem == nodeData) {
-            return 'dhitem-selected';
-          } else {
-            return 'dhitem';
-          }
-        };
+      this.myCollapseAll = function(scope) {
+        console.log("Collapsing...");
+        this.$scope.$broadcast('angular-ui-tree:collapse-all');
+      };
+      this.myExpandAll = function(scope) {
+        this.$scope.$broadcast('angular-ui-tree:expand-all');
+      };
+      this.sureRemove = function(scope) {
+        if (confirm("Are you sure you want to delete this node?")) {
+          scope.remove();
+        }
+      };
 
-        this.itemIcon = function(scope) {
-          if (scope.hasChild()) {
-            return scope.collapsed ? 'glyphicon-folder-close' : 'glyphicon-folder-open';
-          } else {
-            return 'glyphicon-file';
-          }
-        };
-        this.canAdd = function(scope) {
-          return scope.hasChild();
-        };
+      this.toggle = function(scope) {
+        scope.toggle();
+      };
 
+      this.newSubItem = function(scope) {
+        var nodeData = scope.$modelValue;
+        if (nodeData.items == null) {
+          nodeData.items = [];
+        }
 
-        this.myCollapseAll = function(scope) {
-          console.log("Collapsing...");
-          this.$scope.$broadcast('angular-ui-tree:collapse-all');
-        };
-        this.myExpandAll = function(scope) {
-          this.$scope.$broadcast('angular-ui-tree:expand-all');
-        };
-        this.sureRemove = function(scope) {
-          if (confirm("Are you sure you want to delete this node?")) {
-            scope.remove();
-          }
-        };
-
-        this.toggle = function(scope) {
-          scope.toggle();
-        };
-
-        this.newSubItem = function(scope) {
-          var nodeData = scope.$modelValue;
-          if (nodeData.items == null) {
-            nodeData.items = [];
-          }
-
-          var newName = prompt("Name of new item: ");
-          nodeData.items.push({
-            //id: nodeData.id * 10 + nodeData.items.length,
-            id: new Date(),
-            name: newName,
-            items: []
-          });
-        };
+        var newName = prompt("Name of new item: ");
+        nodeData.items.push({
+          //id: nodeData.id * 10 + nodeData.items.length,
+          id: new Date(),
+          name: newName,
+          items: []
+        });
+      };
 
 
-        this.setSelectItem = function(item) {
-          this.episodeListText = "";
-          this.selItem = item;
+      this.setSelectItem = function(item) {
+        this.episodeListText = "";
+        this.selItem = item;
 
-          // If not already done, convert Release Date from ISO string Javascript Date object
-          if (this.selItem.Released) {
-            if (!angular.isDate(this.selItem.Released)) {
-              if (typeof this.selItem.Released == "string") {
-                // Convert ISO 8601 Date string representation ("1999-07-01T00:00:00.000Z") to Javascript Date Object
-                //console.log("Converting date from ISO string to object...");
-                var dtstr = this.selItem.Released.replace(/\D/g, " ");
-                var dtcomps = dtstr.split(" ");
-                dtcomps[1]--;
-                this.selItem.Released = new Date(Date.UTC(dtcomps[0], dtcomps[1], dtcomps[2], dtcomps[3], dtcomps[4], dtcomps[5]));
-              } else {
-                alert("Unknown format for release date of " + this.selItem.name + " : " + this.selItem.Released);
-                throw ("Database format Error");
+        // If not already done, convert Release Date from ISO string Javascript Date object
+        if (this.selItem.Released) {
+          if (!angular.isDate(this.selItem.Released)) {
+            if (typeof this.selItem.Released == "string") {
+              // Convert ISO 8601 Date string representation ("1999-07-01T00:00:00.000Z") to Javascript Date Object
+              //console.log("Converting date from ISO string to object...");
+              var dtstr = this.selItem.Released.replace(/\D/g, " ");
+              var dtcomps = dtstr.split(" ");
+              dtcomps[1]--;
+              this.selItem.Released = new Date(Date.UTC(dtcomps[0], dtcomps[1], dtcomps[2], dtcomps[3], dtcomps[4], dtcomps[5]));
+            } else {
+              alert("Unknown format for release date of " + this.selItem.name + " : " + this.selItem.Released);
+              throw ("Database format Error");
 
-              }
             }
           }
-        };
+        }
+      };
 
-        this.itemSelect = function(scope) {
-          this.editMode = false;
+      this.itemSelect = function(scope) {
+        this.editMode = false;
 
-          if (scope.hasChild()) {
-            this.selItem = scope.$modelValue;
-            this.showShow = false;
-            return;
-          }
+        if (scope.hasChild()) {
+          this.selItem = scope.$modelValue;
+          this.showShow = false;
+          return;
+        }
 
-          // this.listIndex points to where we are in the current branch of items
-          this.listIndex = scope.index();
-          // this.list points to the list of items or within a tree branch
-          this.list = scope.$parentNodeScope.$modelValue;
-          this.showShow = true;
-          this.setSelectItem(scope.$modelValue);
-        };
+        // this.listIndex points to where we are in the current branch of items
+        this.listIndex = scope.index();
+        // this.list points to the list of items or within a tree branch
+        this.list = scope.$parentNodeScope.$modelValue;
+        this.showShow = true;
+        this.setSelectItem(scope.$modelValue);
+      };
 
-        this.showJSONstuff = function() {
-          this.showJSON = true;
-        };
-        this.doneJSON = function() {
-          this.showJSON = false;
-        };
+      this.showJSONstuff = function() {
+        this.showJSON = true;
+      };
+      this.doneJSON = function() {
+        this.showJSON = false;
+      };
 
-        this.nextItem = function() {
+      this.nextItem = function() {
 
-          if (this.listIndex == (this.list.length - 1)) {
-            this.listIndex = 0;
-          } else {
-            this.listIndex++;
-          }
+        if (this.listIndex == (this.list.length - 1)) {
+          this.listIndex = 0;
+        } else {
+          this.listIndex++;
+        }
 
-          this.setSelectItem(this.list.items[this.listIndex]);
-        };
+        this.setSelectItem(this.list.items[this.listIndex]);
+      };
 
-        this.prevItem = function() {
-          if (this.listIndex == 0) {
-            this.listIndex = this.list.length - 1;
-          } else {
-            this.listIndex--;
-          }
-          this.setSelectItem(this.list.items[this.listIndex]);
-        };
+      this.prevItem = function() {
+        if (this.listIndex == 0) {
+          this.listIndex = this.list.length - 1;
+        } else {
+          this.listIndex--;
+        }
+        this.setSelectItem(this.list.items[this.listIndex]);
+      };
 
-        this.dateOptions = {
-          //dateDisabled: disabled,
-          // datepickerMode : 'month',
-          maxDate: new Date(2020, 5, 22),
-          minDate: new Date(1963, 1, 1) // Will not be appropriate when done with videos
-            //startingDay: 1
-        };
+      this.dateOptions = {
+        //dateDisabled: disabled,
+        // datepickerMode : 'month',
+        maxDate: new Date(2020, 5, 22),
+        minDate: new Date(1963, 1, 1) // Will not be appropriate when done with videos
+        //startingDay: 1
+      };
 
 
-        this.open1 = function() {
-          this.popup1.opened = true;
-        };
+      this.open1 = function() {
+        this.popup1.opened = true;
+      };
 
-        this.newEpisode = function() {
-          if (this.selItem.episodes) {
-            this.selItem.episodes.push({
-              name: "",
-              fname: ""
-            });
-          } else {
-            this.selItem.episodes = [{
-              name: "",
-              fname: ""
-            }];
-          }
-        };
-        this.delEpisode = function(index) {
-          this.selItem.episodes.splice(index, 1);
-        };
-        // Return url for use with SRC in HTML5 video
-        this.episodeUrl = function(index) {
-          console.log("returning URL from " +  this.selItem.SPath + "/" + this.selItem.episodes[index].fname);
-          return this.$sce.trustAsResourceUrl(this.selItem.SPath + "/" + this.selItem.episodes[index].fname);
-        };
-
-        this.newDoctor = function(scope) {
-          alert("New Doctor Not Implemented Yet");
-        };
-
-        this.getDescrLink = function() {
-          if (this.selItem.descrUrl) {
-            return this.selItem.descrUrl
-          } else {
-            return '#';
-          }
-        };
-        this.backup = function() {
-          var c2String = angular.toJson(this.videoList);
-
-          $http({
-            method: 'POST',
-            url: '/api/backups/',
-            data: {
-              "c2_data": c2String,
-              "bkfile": "drwho_tv_tree"
-            }
-          }).success(function(data) {
-            alert("Backup Successful!\n\n" + data);
-          }).error(function(data, status, headers, config) {
-            // Handle the error
-            alert("Backup failed with status: " + status);
+      this.newEpisode = function() {
+        if (this.selItem.episodes) {
+          this.selItem.episodes.push({
+            name: "",
+            fname: ""
           });
+        } else {
+          this.selItem.episodes = [{
+            name: "",
+            fname: ""
+          }];
+        }
+      };
+      this.delEpisode = function(index) {
+        this.selItem.episodes.splice(index, 1);
+      };
+      // Return url for use with SRC in HTML5 video
+      this.episodeUrl = function(index) {
+        console.log("returning URL from " + this.selItem.SPath + "/" + this.selItem.episodes[index].fname);
+        return this.$sce.trustAsResourceUrl(this.selItem.SPath + "/" + this.selItem.episodes[index].fname);
+      };
 
-        };
-      } // end constructor
+      this.newDoctor = function(scope) {
+        alert("New Doctor Not Implemented Yet");
+      };
+
+      this.getDescrLink = function() {
+        if (this.selItem.descrUrl) {
+          return this.selItem.descrUrl
+        } else {
+          return '#';
+        }
+      };
+      this.backup = function() {
+        var c2String = angular.toJson(this.videoList);
+
+        $http({
+          method: 'POST',
+          url: '/api/backups/',
+          data: {
+            "c2_data": c2String,
+            "bkfile": "drwho_tv_tree"
+          }
+        }).success(function(data) {
+          alert("Backup Successful!\n\n" + data);
+        }).error(function(data, status, headers, config) {
+          // Handle the error
+          alert("Backup failed with status: " + status);
+        });
+
+      };
+    } // end constructor
 
     $onInit() {
-        this.$http.get('drwho_tv_tree.json', {
-          cache: true
-        }).then(response => {
-          console.log("Got Dr Who video data");
-          //this.ngToast.create('Got Dr Who video data');
-          this.videoList = response.data;
-        }, function errorCallback(response) {
-          alert("Request for Dr Who video data yielded error: " + response.status);
-        });
-      } // end onInit
+      this.$http.get('drwho_tv_tree.json', {
+        cache: true
+      }).then(response => {
+        console.log("Got Dr Who video data");
+        //this.ngToast.create('Got Dr Who video data');
+        this.videoList = response.data;
+      }, function errorCallback(response) {
+        alert("Request for Dr Who video data yielded error: " + response.status);
+      });
+    } // end onInit
   } // end component class
 
   angular.module('myappApp')
