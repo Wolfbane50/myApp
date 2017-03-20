58c2081aeb6cac844dccc94f29f826784f0422e6
Daniel Heaney <Daniel Heaney>
2017-03-19 20:19:49 -0400

    Good man goes to war

--- partial.bat
+++ partial.bat
@@ -1,3 +1,2 @@
-cd "C:\Users\Daniel\Downloads\Series 6"
-C:\ffmpeg-20160310-git-66edd86-win64-static\bin\ffmpeg -i "VTS_02_VOBID_001_1.VOB" "Doctor Who - s06e005 The Rebel Flesh.mp4"
-C:\ffmpeg-20160310-git-66edd86-win64-static\bin\ffmpeg -i "VTS_02_VOBID_002_1.VOB" "Doctor Who - s06e006 The Almost People.mp4"
+cd "C:\Users\Daniel\Downloads\Doctor Who S06E07 A Good Man Goes to War - XviD"
+C:\ffmpeg-20160310-git-66edd86-win64-static\bin\ffmpeg -i "Doctor Who S06E07 A Good Man Goes to War.avi" "Doctor Who - s06e007 A Good Man Goes to War.mp4"
