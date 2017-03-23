8fa414e87d4ad480eec63cf1fbe3de2c3481bbf0
Daniel Heaney <Daniel Heaney>
2017-03-23 06:21:26 -0400

    Rest of Season 6

--- partial.bat
+++ partial.bat
@@ -1,2 +1,4 @@
-cd "C:\Users\Daniel\Downloads\Doctor Who S06E07 A Good Man Goes to War - XviD"
-C:\ffmpeg-20160310-git-66edd86-win64-static\bin\ffmpeg -i "Doctor Who S06E07 A Good Man Goes to War.avi" "Doctor Who - s06e007 A Good Man Goes to War.mp4"
+cd "C:\Users\Daniel\Downloads\Season 6"
+C:\ffmpeg-20160310-git-66edd86-win64-static\bin\ffmpeg -i "VTS_02_VOBID_001_1.VOB" "Doctor Who - s06e011 The God Complex.mp4"
+C:\ffmpeg-20160310-git-66edd86-win64-static\bin\ffmpeg -i "VTS_02_VOBID_002_1.VOB" "Doctor Who - s06e012 Closing Time.mp4"
+C:\ffmpeg-20160310-git-66edd86-win64-static\bin\ffmpeg -i "VTS_02_VOBID_003_1.VOB" "Doctor Who - s06e013 The Wedding of River Song.mp4"
