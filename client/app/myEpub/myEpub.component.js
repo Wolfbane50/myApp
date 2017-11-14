37df687ccddf9838abf5f1058a29856cc4978cf7
Daniel Heaney <daniel.heaney@gmail.com>
2017-11-13 21:32:51 -0500

    More epub stuff

--- client/app/myEpub/myEpub.component.js
+++ client/app/myEpub/myEpub.component.js
@@ -6,8 +6,9 @@
         this.$http = $http;
         this.$state = $state;
         this.$scope = $scope;
-        this.bookUrl = "epub/reader/moby-dick/";
-      //  this.bookUrl = "https://s3.amazonaws.com/moby-dick/";
+        //this.bookUrl = "epub/Beginning.Android.3.epub";
+
+        this.bookUrl = "epub/86e2n.B.epub";
         this.chapters = [];
         this.Book;
         this.loadingBook = true;
@@ -17,6 +18,14 @@
         //=================================
         this.changeBook = function() {
             console.log("Changing Book...");
+            var newBookUrl = prompt("URL of epub to view_container: ");
+            if (newBookUrl) {
+              this.chapters = [];
+              this.Book.destroy();
+              this.bookUrl = newBookUrl;
+              this.openBook(this.bookUrl);
+            }
+
         };
         this.selectedClass = function(index) {
           if (this.selectedChapter === index ) {
@@ -35,39 +44,54 @@
         this.whereAmI = function() {
           alert(this.Book.getCurrentLocationCfi());
         };
+        this.openBook = function(url) {
+          console.log("this.openBook => " + url);
+          this.loadingBook = true;
+          this.Book = ePub({
+                    restore: true,
+                    withCredentials: true
+          //          layout: "pre-paginated"
+
+                  });
+                  this.Book.forceSingle();
+          //this.bookUrl = "epub/Beginning.Android.3.epub";
+          this.Book.open(url);
+
+          console.log("Getting TOC");
+          var ctrl = this;
+          this.Book.getToc().then(function(toc) {
+              console.log("Got TOC");
+              ctrl.chapters = toc;
+            });
+
+            this.Book.ready.all.then(function() {
+              console.log("Ready all...");
+              ctrl.loadingBook = false;
+              console.log("Ready all after set flag...");
+            });
+
+
+            this.Book.renderTo("epubarea").then(function() {
+              console.log("Book rendered...");
+            }, function(error) {
+              console.log("Book rendered error path")
+              console.error(error);
+            });
+
+        };
 
       } // end constructor
 
 
       $onInit() {
         console.log("Instantiating Book");
-        this.Book = ePub(this.bookUrl, {
+//        this.Book = ePub(this.bookUrl, {
 //          width: "400px",
 //          height: "600px",
-          spreads: false
+//          spreads: false,
+        console.log("Calling openBook");
+        this.openBook(this.bookUrl);
 
-        });
-
-        console.log("Getting TOC");
-        var ctrl = this;
-        this.Book.getToc().then(function(toc) {
-            console.log("Got TOC: " +  JSON.stringify(toc));
-            ctrl.chapters = toc;
-          });
-
-          this.Book.ready.all.then(function() {
-            console.log("Ready all...");
-            ctrl.loadingBook = false;
-            console.log("Ready all after set flag...");
-          });
-
-
-          this.Book.renderTo("epubarea").then(function() {
-            console.log("Book rendered...");
-          }, function(error) {
-            console.log("Book rendered error path")
-            console.error(error);
-          });
         } // end onInit
       } // end component class
 
