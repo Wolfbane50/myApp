8dab136721a39733f80c22d245274408ffedcea6
Daniel Heaney <daniel.heaney@gmail.com>
2018-08-17 15:19:58 -0400

    Misc stuff

--- DH_scripts/extractTor.js
+++ DH_scripts/extractTor.js
@@ -1,6 +1,7 @@
+var fs = require('fs');
+var Feedly = require('feedly');
 
-
-function getTorNumber (url) {
+function getTorNumber(url) {
   if (url.match(/yourbittorrent.com/)) {
     var torNum = url.replace(/https:\/\/yourbittorrent.com\/torrent\//, "");
     torNum = torNum.replace(/\/.*$/, "");
@@ -9,5 +10,26 @@ function getTorNumber (url) {
   }
 }
 
-var inpUrl = "https://yourbittorrent.com/torrent/13085960/video-post-%E2%80%93-video-sharing-html-template.html";
-console.log("Number: " + getTorNumber(inpUrl));
+//ar inpUrl = "https://yourbittorrent.com/torrent/13085960/video-post-%E2%80%93-video-sharing-html-template.html";
+//console.log("Number: " + getTorNumber(inpUrl));
+
+function getTorrents() {
+  const f = new Feedly({
+    client_id: 'f402f196-d4dd-4471-ab35-d8c8a7de8d23',
+    client_secret: 'AzZ6asvwdFmkZv2ATbxwIg7LE5CUSj19nCZfWUbDdEx4YANfb8vNdr3Zd0LLUb4Dq_b38K8hxoeIGQoElHGi-Lb4zmz7iMcdc_4340JeNlDtkXnZMltlnhj-sWOCGq6Hdib71SHv8xn670BF_NWdor7OHTchqw1rycJON4ZuIf9v9wYMNla-O3MoTsTzP-fG7vS_SDFqUMP3Zhl0e-3Hrgl5nv3v2LBwhjMiyJwZedp0WwbZ9du0iImcvUNDHA:feedlydev',
+    port: 8080
+  });
+
+
+  // (promise(Array(Subscription))) subscriptions(cb)
+  f.subscriptions().then(function(results) {
+      console.log(JSON.stringify(results));
+      // process results
+    },
+    function(error) {
+      // process error
+      console.log("Call to subscriptions failed ==> " + error);
+    });
+}
+
+getTorrents();
--- server/api/saved_tors/saved_tors.controller.js
+++ server/api/saved_tors/saved_tors.controller.js
@@ -9,7 +9,9 @@
 
 'use strict';
 //import fs from 'fs';
-const Feedly = require('feedly')
+const Feedly = require('feedly');
+
+// Your user id is f402f196-d4dd-4471-ab35-d8c8a7de8d23.
 
 export function getTorNumber (url) {
   if (url.match(/yourbittorrent.com/)) {
@@ -21,10 +23,19 @@ export function getTorNumber (url) {
 }
 export function getTorrents(req, res) {
   const f = new Feedly({
-    client_id: 'MY_CLIENT_ID',
-    client_secret: 'MY_CLIENT_SECRET',
+    client_id: 'f402f196-d4dd-4471-ab35-d8c8a7de8d23',
+    client_secret: 'AzZ6asvwdFmkZv2ATbxwIg7LE5CUSj19nCZfWUbDdEx4YANfb8vNdr3Zd0LLUb4Dq_b38K8hxoeIGQoElHGi-Lb4zmz7iMcdc_4340JeNlDtkXnZMltlnhj-sWOCGq6Hdib71SHv8xn670BF_NWdor7OHTchqw1rycJON4ZuIf9v9wYMNla-O3MoTsTzP-fG7vS_SDFqUMP3Zhl0e-3Hrgl5nv3v2LBwhjMiyJwZedp0WwbZ9du0iImcvUNDHA:feedlydev',
     port: 8080
   });
 
 
+// (promise(Array(Subscription))) subscriptions(cb)
+  f.subscriptions().then(function(results) {
+    console.log(JSON.stringify(results));
+    // process results
+},
+function (error) {
+    // process error
+    console.log("Call to subscriptions failed ==> " + error);
+});
 }
