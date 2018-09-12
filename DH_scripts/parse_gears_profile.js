43df99869f577a19f003eb012d2221f8a3e6995f
Heaney <1274185364@mil>
2018-09-12 16:42:48 -0400

    Complete gears mixin work

--- DH_scripts/parse_gears_profile.js
+++ DH_scripts/parse_gears_profile.js
@@ -1,19 +1,69 @@
 // parse_chachi_excel.js
 var all_mixins = {};
 var ffg_mixins = [];
+var tree_array = {};
+var applicableBaselines = [];
 
+searchItems = function(term, list) {
+  for (var i = 0; i < list.length; i++) {
+    if (list[i].name === term) {
+      return i;
+    }
+  }
+  return -1;
+};
 
-
-addEntity = function(mixin) {
+addEntity = function(mixin, idx) {
   var entities = mixin.split(/\./);
   //console.log(JSON.stringify(entities));
 
   var entryPoint = all_mixins;
-  for (i=0; i<entities.length; i++) {
+  var treePtr = tree_array;
+  for (i = 0; i < entities.length; i++) {
+    //console.log("Working on " + entities[i] + " of " + mixin);
+    var entity = {
+      "name": entities[i],
+      "fullname": mixin
+    };
+
     if (entryPoint[entities[i]]) {} else {
-      entryPoint[entities[i]] ={};
+      entryPoint[entities[i]] = {};
     }
     entryPoint = entryPoint[entities[i]];
+    if (!treePtr.items) {
+      treePtr.items = [entity];
+      treePtr = treePtr.items[0]
+    } else {
+      var index = searchItems(entities[i], treePtr.items)
+      if (index == -1) {
+        // Not found in items, so add it
+        treePtr.items.push(entity);
+        treePtr = treePtr.items[treePtr.items.length - 1];
+      } else {
+        // entity exists in items
+        treePtr = treePtr.items[index];
+      }
+    }
+  }
+  return treePtr;
+};
+
+addApplicability = function(recordPtr, row) {
+  var exceptions = {
+    "Mixin": true,
+    "Doors Count": true,
+    "CPP": true,
+    "Make": true,
+    "XML": true,
+    "Drist": true,
+    "Table": true,
+    "ADA": true
+  };
+  for (col in row) {
+    if (exceptions[col]) {
+      continue;
+    }
+    recordPtr[col] = row[col];
   }
 };
 
@@ -36,24 +86,40 @@ parse_gears_excel = function() {
 
     if (idSheets[sheet_name]) {
       var worksheet = workbook.Sheets[sheet_name];
+      //console.log("Worksheet range = " + worksheet['!ref']);  // => A1:CS666
+
+      // Get all the Baseline namesArr
+      var blObj = XLSX.utils.sheet_to_json(worksheet, {
+        header: 1,
+        range: "I2:CS2"
+      });
+      applicableBaselines = blObj[0];
+      for (var j=0; j<8; j++) {
+        applicableBaselines.shift();
+      }
+      //console.log(JSON.stringify(applicableBaselines));
+      tree_array.applicableBaselines = applicableBaselines;
+
 
       // Parse through rows
       var roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name], {
-        range: 2,
-        header: "A"
+        range: 1
       });
       var row_num = 0;
       //console.log(JSON.stringify(roa));
       roa.forEach(function(row) {
         //console.log(JSON.stringify(row));
         //console.log(row.A + ' ' + (row.AQ ? "FFG" : ''));
-        addEntity(row.A);
-        if(row.AQ) {
-          ffg_mixins.push(row.A);
+        recordPtr = addEntity(row.Mixin, row_num);
+        //console.log("For " + row.Mixin + ", Added " + JSON.stringify(recordPtr));
+        addApplicability(recordPtr, row);
+        if (row.FFGX) {
+          ffg_mixins.push(row.Mixin);
         }
+        row_num++;
       });
       // Save last set on sheet
-      console.log(JSON.stringify(all_mixins));
+      //console.log(JSON.stringify(all_mixins));
       //saveSet(chachi_set);
     }
   }
@@ -63,5 +129,7 @@ parse_gears_excel();
 var fs = require('fs');
 var s_path = "server/public/";
 //console.log('Updating Chachi.json');
+//console.log(JSON.stringify(applicabilityTruthTable));
 fs.writeFile(s_path + 'mixins.json',
-  JSON.stringify(all_mixins));
+  JSON.stringify(tree_array));
+//  JSON.stringify(all_mixins));
--- client/app/gearsMixins/gearsMixins.component.js
+++ client/app/gearsMixins/gearsMixins.component.js
@@ -1,43 +1,92 @@
 'use strict';
 
 (function() {
-    class GearsMixinsComponent {
-      constructor($http, $state, $scope) {
-        this.$http = $http;
-        this.$state = $state;
-        this.$scope = $scope;
+  class GearsMixinsComponent {
+    constructor($http, $state, $scope) {
+      this.$http = $http;
+      this.$state = $state;
+      this.$scope = $scope;
 
-        this.mixins = {};
+      this.mixins = {};
+      this.selItem = {};
 
-        //=================================
-        // Scope methods
-        //=================================
-        this.showJSONstuff = function() {
-             this.showJSON = true;
-        };
+      this.FFGApplicable = false;
+      this.Bl10Applicable = false;
+      this.otherApplicable = false;
 
-        this.doneJSON = function() {
-             this.showJSON = false;
-        };
-      } // end constructor
+      // Just a sample for now
+      this.configurations = [ "None" ];
+      this.configIndex = 0;
+      this.configName = this.configurations[this.configIndex];
+
+      //=================================
+      // Scope methods
+      //=================================
+      this.showJSONstuff = function() {
+        this.showJSON = true;
+      };
+
+      this.doneJSON = function() {
+        this.showJSON = false;
+      };
+
+      this.selectedItemClass = function(scope) {
+        var nodeData = scope.$modelValue;
+        if (this.selItem == nodeData) {
+          return 'dhitem-selected';
+        } else {
+          return 'dhitem';
+        }
+      };
+
+      this.itemIcon = function(scope) {
+        if (scope.hasChild()) {
+          return scope.collapsed ? 'glyphicon-folder-close' : 'glyphicon-folder-open';
+        } else {
+          return 'glyphicon-file';
+        }
+      };
+
+      this.myCollapseAll = function() {
+        $scope.$broadcast('angular-ui-tree:collapse-all');
+      };
+      this.myExpandAll = function() {
+        $scope.$broadcast('angular-ui-tree:expand-all');
+      };
+
+      this.toggle = function(scope) {
+        scope.toggle();
+      };
+
+      this.itemSelect = function(scope) {
+        this.selItem = scope.$modelValue;
+        //alert("Selected item:  " + this.selItem.name);
+        this.FFGApplicable = (this.selItem["FFGX"]) ? true : false;
+        this.Bl10Applicable = (this.selItem["AEGIS_BASELINE_10_C0_0_0_2G"]) ? true : false;
+        this.otherApplicable = (this.selItem[this.configName]) ? true : false;
+      };
 
 
-      $onInit() {
-        this.$http.get('mixins.json', {
-          cache: true
-        }).then(response => {
-        //alert("Got c2 tree data ");
-           this.mixins = response.data;
-        }, function errorCallback(response) {
-            alert("Request for mixins data yielded error: " + response.status);
-        });
-        } // end onInit
-      } // end component class
+    } // end constructor
 
-      angular.module('myappApp')
-        .component('gearsMixinsComponent', {
-          templateUrl: 'app/gearsMixins/gearsMixins.html',
-          controller: GearsMixinsComponent
-        });
 
-    })();
+    $onInit() {
+      this.$http.get('mixins.json', {
+        cache: true
+      }).then(response => {
+        //lert("Got c2 tree data ");
+        this.mixins = response.data.items;
+        this.configurations = response.data.applicableBaselines;
+      }, function errorCallback(response) {
+        alert("Request for mixins data yielded error: " + response.status);
+      });
+    } // end onInit
+  } // end component class
+
+  angular.module('myappApp')
+    .component('gearsMixinsComponent', {
+      templateUrl: 'app/gearsMixins/gearsMixins.html',
+      controller: GearsMixinsComponent
+    });
+
+})();
