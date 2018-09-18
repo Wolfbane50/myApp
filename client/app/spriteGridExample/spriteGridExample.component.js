55bebaadf9eb6dc3f45a0655d998454491db7d3d
Daniel Heaney <daniel.heaney@gmail.com>
2018-09-18 14:25:18 -0400

    Show the sprite table

--- client/app/spriteGridExample/spriteGridExample.component.js
+++ client/app/spriteGridExample/spriteGridExample.component.js
@@ -3,19 +3,35 @@
 (function(){
 
 class SpriteGridExampleComponent {
-  constructor() {
+  constructor($http, $scope) {
+    this.$http = $http;
     this.Doc = "";
 
     this.Featuring = "Adric, Nyssa";
     this.blah = "3rd, War";
-  }
+    this.featureTable = [];
+  }  // end constructor
+  $onInit() {
+      //console.log("spritespec.min.json BF Data");
+      this.$http.get('spriteSpec.min.json', {
+        cache: true
+      }).then(response => {
+        //console.log("Got response => " + JSON.stringify(response));
+        this.featureTable = response.data.spriteTable;
+        this.aliases = response.data.aliasMap;
+      }, function errorCallback(response) {
+          alert("Request for feature data yielded error: " + response.status);
+
+      });
+    } // end of $onInit
+
+
 }
 
 angular.module('myappApp')
   .component('spriteGridExampleComponent', {
     templateUrl: 'app/spriteGridExample/spriteGridExample.html',
-    controller: SpriteGridExampleComponent,
-    controllerAs: 'SpriteGridExample'
+    controller: SpriteGridExampleComponent
   });
 
 })();
